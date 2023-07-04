847c20de3436c8afb9407489cf4435f78d39af83
Dirk Mueller <dirkmuller8@outlook.de>
2023-07-04 13:43:42 +0200

    Deleted boilerplate in HC and cretaed public class GuestResponse

---
+++ Models/GuestResponse.cs
@@ -0,0 +1,10 @@
+namespace ch3_dotnet7.Models
+{
+    public class GuestResponse
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
