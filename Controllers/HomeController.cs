252324cff2c8d62f9b80a042f3edc44f34bac809
Dirk Mueller <dirkmuller8@outlook.de>
2023-07-04 14:58:09 +0200

    Added Click <a asp-action='RsvpForm'>here</a> to Index.cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,7 +10,7 @@ public class HomeController : Controller
     {
         return View();
     }
-    public IActionResult RsvpForm()
+    public ViewResult RsvpForm()
     {
         return View();
     }
